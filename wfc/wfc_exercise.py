2608e8f26ffe0eb6b9cec9d8ab7010e7b8c61d0e
PrimtalPrimdahl <ezpthaulow@gmail.com>
2024-02-11 16:51:14 +0100

    Improved solution with dictionary

--- wfc/wfc_exercise.py
+++ wfc/wfc_exercise.py
@@ -18,11 +18,15 @@ from random import randint, shuffle, uniform
 from queue import Queue, LifoQueue
 from time import time
 import traceback 
+import time
 
 
 # The East North West South vector contains index pairs for
 # adjacent cells.
 ENWS = [[1,0],[0,1],[-1,0],[0,-1]]
+GRID_SIZE = 20
+use_queue = False
+use_dict = True
 
 
 def ingrid(g, i, j):
@@ -86,8 +90,21 @@ def load_tiles(exemplar):
 # -------------------------------------
 # Here the tile set is loaded, so change line below to try other
 # tile set.
-tiles = load_tiles("Knots")
+tiles = load_tiles("Rooms")
 # -------------------------------------
+
+#List which stores uncollapsed cells
+wave_dict= dict()
+
+def create_wave_dict(wave_grid):
+    d = dict()
+    for i in range(wave_grid.shape[0]):
+        for j in range(wave_grid.shape[1]):
+            if(sum(wave_grid[i,j]) > 1):
+                d[(i,j)] = 1
+    return d
+
+
 
 def pick_tile(we):
     '''from an array of possible tiles (0's and 1's) choose an entry containing 1
@@ -99,6 +116,11 @@ def pick_tile(we):
     x = randint(0,len(l)-1)
     we_new = zeros(len(we), dtype=int)
     we_new[l[x]] = 1
+    return we_new
+
+def pick_specific_tile(we, index):
+    we_new = zeros(len(we), dtype=int)
+    we_new[index] = 1
     return we_new
 
 def wave_grid_to_image(wave_grid):
@@ -116,30 +138,80 @@ def wave_grid_to_image(wave_grid):
     return I
 
 
-def observe(wave_grid):
+def observe_no_dict(wave_grid):
     '''The observe function picks a tile of minimum entropy and collapses
     its part of the wave function. A tuple with the tile indices is returned.'''
     #Choose random tile in grid 
     i, j = randint(0,wave_grid.shape[0]-1), randint(0,wave_grid.shape[1]-1)
 
     # Student code begin ---------------
-    #Find lowest entropy cell in wave_grid
-    for row in range(0, wave_grid.shape[0]-1):
-        for col in range(0,wave_grid.shape[1]-1):
-            # if this tile has lower entropy we should save those indecies 
-            if((sum(wave_grid[i, j]) == 0) or sum(wave_grid[i, j]) > sum(wave_grid[row, col]) and sum(wave_grid[row, col]) > 1 ):
-                i = row
-                j = col
+    min_entropy = 100000
+    min_entropy_cells = []
+    index = 0
 
-    # Now we should have a min entropy grid index that's not 1 (Meaning we havent collapsed this one yet)
-    #print("Chosen tile with index %d %d and entropy %d" % (i, j, sum(wave_grid[i, j])))
+    #Find lowest entropy cell in wave_grid
+    for row in range(0, wave_grid.shape[0]):
+        for col in range(0,wave_grid.shape[1]):
+            # if this tile has lower entropy we should save those indecies 
+            curr_entropy = sum(wave_grid[row, col])
+            if(curr_entropy > 1):
+                if(curr_entropy == min_entropy):
+                    min_entropy_cells.append([row,col])
+                elif(curr_entropy < min_entropy):
+                    min_entropy = curr_entropy
+                    min_entropy_cells.clear()
+                    min_entropy_cells.append([row,col])
+    
+    # Randomly choose index in min_entropy_cells
+    if(len(min_entropy_cells) == 0):
+        return -1,-1
+    elif(len(min_entropy_cells) > 1):
+        index = randint(0,len(min_entropy_cells)-1)
+        
+    i,j = min_entropy_cells[index]
 
     # Student code end ---------------
     wave_grid[i, j, :] = pick_tile(wave_grid[i, j, :]) #Observe and collapse this tile and return the index of the newly collapsed tile
+
+    return i, j
+
+def observe_dict(wave_grid):
+    '''The observe function picks a tile of minimum entropy and collapses
+    its part of the wave function. A tuple with the tile indices is returned.'''
+    #Choose random tile in grid 
+    i, j = randint(0,wave_grid.shape[0]-1), randint(0,wave_grid.shape[1]-1)
+
+    # Student code begin ---------------
+    min_entropy = 100000
+    min_entropy_cells = []
+    index = 0
+
+    for key in wave_dict:
+        curr_entropy = sum(wave_grid[key[0], key[1]])
+        if (curr_entropy == min_entropy):
+            min_entropy_cells.append([key[0],key[1]])
+        elif(curr_entropy < min_entropy):
+            min_entropy = curr_entropy
+            min_entropy_cells.clear()
+            min_entropy_cells.append([key[0],key[1]])
+    
+    # Randomly choose index in min_entropy_cells
+    if(len(min_entropy_cells) == 0):
+        return -1,-1
+    elif(len(min_entropy_cells) > 1):
+        index = randint(0,len(min_entropy_cells)-1)
+        
+    i,j = min_entropy_cells[index]
+
+    # Student code end ---------------
+    wave_grid[i, j, :] = pick_tile(wave_grid[i, j, :]) #Observe and collapse this tile and return the index of the newly collapsed tile
+
+    wave_dict.pop((i,j))
+
     return i, j
 
 
-def propagate(wave_grid, i, j):
+def propagate_queue(wave_grid, i, j):
     '''Propagates the changes when a cell has been observed.'''
     # Student code begin ---------------
     #print("Propergating")
@@ -152,31 +224,30 @@ def propagate(wave_grid, i, j):
         #print("Going through queue")
         i,j = q.get()
 
-
         #Iterate through neighboring offsets 
         for d, n_off in enumerate(ENWS):
             #print("Going through neighbors")
 
             ni, nj = i + n_off[0], j + n_off[1] #Get neighbor index 
             if ingrid(wave_grid, ni, nj): # If the neighbor is inside the grid 
-                #print("Neighbor was in grid %d %d" % (ni,nj))
 
                 #Get current superposition 
                 currSuperpos = sum(wave_grid[ni, nj])
-                #Remove no longer viable tiles 
 
                 #Go though tiles in neighbor
                 for idx_n, tile in enumerate(wave_grid[ni,nj]):
 
+                    if(tile == 0):
+                        continue
+
                     hasConnection = False
-                    #print("Going through tiles in neighboer, curr idx %d" % idx_n)
+                   
                     # Check if it is compatible with the chosen tile for i,j
 
                     # Go through center tile and check neighbor tiles 
                     for idx_c, t1 in enumerate(wave_grid[i,j]):
-                        #print("Going through tiles in center tile %d" % idx_c)
-
                         if(t1 == 1): #See if we have connection to this tile in this direction
+
                             hasConnection = (tiles[idx_c].connections[d, idx_n] == 1)
 
                             #We don't have to keep going since there is a match
@@ -189,23 +260,77 @@ def propagate(wave_grid, i, j):
                         wave_grid[ni,nj,idx_n] = 0 
 
 
+                if(sum(wave_grid[ni,nj]) == 0):
+                    print("NEIGHBOR IS 0")
+                    return
                 #Add the neighbor to the queue if we have connections to it and its superpositions got reduces  
                 if(sum(wave_grid[ni,nj]) < currSuperpos):
                     q.put((ni,nj))
+                
+    # Student code end ---------------
+                    
+def propagate_no_queue(wave_grid, i, j):
+    '''Propagates the changes when a cell has been observed.'''
+    # Student code begin ---------------
+    
+    #Iterate through neighboring offsets 
+    for d, n_off in enumerate(ENWS):
 
-                           
-    #print("Done propergating")
+        ni, nj = i + n_off[0], j + n_off[1] #Get neighbor index 
+        if ingrid(wave_grid, ni, nj): # If the neighbor is inside the grid 
+
+            #Go though tiles in neighbor
+            for idx_n, tile in enumerate(wave_grid[ni,nj]):
+
+                if(tile == 0):
+                    continue
+
+                hasConnection = False
+                
+                # Check if it is compatible with the chosen tile for i,j
+
+                # Go through center tile and check neighbor tiles 
+                for idx_c, t1 in enumerate(wave_grid[i,j]):
+                    if(t1 == 1): #See if we have connection to this tile in this direction
+
+                        hasConnection = (tiles[idx_c].connections[d, idx_n] == 1)
+
+                        #We don't have to keep going since there is a match
+                        if(hasConnection):
+                            break
+
+
+                #If we don't have a connection to this tile remove it 
+                if(not hasConnection):
+                    wave_grid[ni,nj,idx_n] = 0 
+
+
+            if(sum(wave_grid[ni,nj]) == 0):
+                print("NEIGHBOR IS 0")
+                return      
     # Student code end ---------------
 
 
 def WFC(wave_grid):
     try:
-        i, j = observe(wave_grid)
-        propagate(wave_grid, i, j)
+        if(use_dict):
+            i, j = observe_dict(wave_grid)
+        else:
+            i,j = observe_no_dict(wave_grid)
+
+        if(use_queue):
+            propagate_queue(wave_grid, i, j)
+        else:
+            propagate_no_queue(wave_grid, i, j)
+
+        if(len(wave_dict) == 0):
+            return False
+        if(i == -1 and j == -1):
+            return False
         return True
     except Exception as e:
+        traceback.print_stack
         print(e)
-        traceback.print_exc() 
         return False
 
 
@@ -243,20 +368,43 @@ def run(wave_grid):
 # you can run the code below to produce a texture.
 # Try with a number of tilesets. The resulting images
 # are submitted. Try at least "Knots" and "FloorPlan"
-wave_grid = ones((15,15,len(tiles)), dtype=int)
-run_interactive(wave_grid)
+wave_grid = ones((GRID_SIZE,GRID_SIZE,len(tiles)), dtype=int)
+
+#run_interactive(wave_grid)
 #run(wave_grid)
 
 # Part 2: Introduce constraints by precollapsing one or more 
 # cells to admit only one or more tiles in these cells. Discuss 
 # what you are trying to achieve and submit the discussion along 
 # with the resulting images.
+idx = int((GRID_SIZE-1)/2)
+#wave_grid[idx, idx, :] = pick_tile(wave_grid[idx, idx, :])
+wave_grid[idx, idx] = pick_specific_tile(wave_grid[idx,idx],19)
+wave_grid[idx+1, idx] = pick_specific_tile(wave_grid[idx+1,idx],19)
+wave_grid[idx, idx+1] = pick_specific_tile(wave_grid[idx,idx+1],19)
+wave_grid[idx+1, idx+1] = pick_specific_tile(wave_grid[idx+1,idx+1],19)
+
+wave_dict = create_wave_dict(wave_grid)
+
+start = time.time()
+run_interactive(wave_grid)
+end = time.time()
+
+print(end-start)
 
 # Part 3: Change your propagate function such that only adjacent
 # cells are updated. Use this to produce a new texture based
 # on FloorPlan. Does this change the result? If so how? Show
 # images. 
 
+#DONE
 # Part 4 (NON-MANDATORY AND PROBABLY HARD)
 # Input a single image and make the tileset from patches in this
 # image. See if you can produce results similar to Marie's
+
+
+#Get image
+
+#Create tiles 
+
+
